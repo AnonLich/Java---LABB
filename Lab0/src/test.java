f740ad22ba246ff84f9d3704dfe1bb277c77a1e6
Melvin <melvin_02@hotmail.se>
2023-01-16 16:43:18 +0100

    PrintarNummer

--- Lab0/src/test.java
+++ Lab0/src/test.java
@@ -1,10 +1,33 @@
 
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-        System.out.println("Hello, World!"); 
+	
+    public static boolean isValidPNR(String number) {
+    	
+    	int siffra = 0;
+		for (int i = 0; i < number.length(); i++) {
+    		if (i % 2 == 0) {
+    			siffra = (Character.digit(number.charAt(i), 10) * 2);
 
-	}
+    		} else {
+    			siffra = Character.digit(number.charAt(i), 10);
+    		}
+			System.out.println(siffra);
+		}
+    			return false;
+    	
+	
+    }
+    
 
-}
+		    public static void main(String[] args) {
+		        String personNummer = "9103259876";
+		        
+
+		        if (isValidPNR(personNummer)) {
+		            System.out.println("Giltigt nummer.");
+		        } else {
+		            System.out.println("Ej ett giltigt nummer.");
+		        }
+		    }
+		}
